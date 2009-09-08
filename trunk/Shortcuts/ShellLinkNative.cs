fe462638d44407cd25124317de325cdfa3126e2d
wyattoday <wyattoday@5120c706-1a6c-11de-8b5c-13accb87f508>
2009-09-08 08:15:09 +0000

    Get rid of Ansi struct

--- trunk/Shortcuts/ShellLinkNative.cs
+++ trunk/Shortcuts/ShellLinkNative.cs
@@ -29,24 +29,6 @@ namespace wyUpdate
         SLGP_RAWPATH = 0x4
     }
 
-    [StructLayoutAttribute(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
-    public struct WIN32_FIND_DATAA
-    {
-        public int dwFileAttributes;
-        public System.Runtime.InteropServices.ComTypes.FILETIME ftCreationTime;
-        public System.Runtime.InteropServices.ComTypes.FILETIME ftLastAccessTime;
-        public System.Runtime.InteropServices.ComTypes.FILETIME ftLastWriteTime;
-        public int nFileSizeHigh;
-        public int nFileSizeLow;
-        public int dwReserved0;
-        public int dwReserved1;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = MAX_PATH)]
-        public string cFileName;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 14)]
-        public string cAlternateFileName;
-        private const int MAX_PATH = 260;
-    }
-
     [StructLayoutAttribute(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
     public struct WIN32_FIND_DATAW
     {
