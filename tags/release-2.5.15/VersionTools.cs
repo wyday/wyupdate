042cab0d31670803b9d04a4df810a88f7a68a0d1
wyattoday <wyattoday@5120c706-1a6c-11de-8b5c-13accb87f508>
2010-02-05 19:17:08 +0000

    tag 2.5.15

---
+++ tags/release-2.5.15/VersionTools.cs
@@ -0,0 +1,323 @@
+﻿using System.Collections;
+using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace wyUpdate.Common
+{
+    public static class VersionTools
+    {
+        static readonly Hashtable greek_ltrs = new Hashtable
+	        {
+		        {"alpha", 0}, {"beta", 1}, {"gamma", 2},
+                {"delta", 3}, {"epsilon", 4}, {"zeta", 5},
+                {"eta", 6}, {"theta", 7}, {"iota", 8},
+                {"kappa", 9}, {"lambda", 10}, {"mu", 11},
+                {"nu", 12}, {"xi", 13}, {"omicron", 14},
+                {"pi", 15}, {"rho", 16}, {"sigma", 17},
+                {"tau", 18}, {"upsilon", 19}, {"phi", 20},
+                {"chi", 21}, {"psi", 22}, {"omega", 23},
+                {"rc", 24} // RC = release candidate
+	        };
+
+        /// <summary>
+        /// Compares two specified string versions and returns an integer that indicates their relationship in the sort order.
+        /// </summary>
+        /// <param name="versionA">The first verison to compare</param>
+        /// <param name="versionB">The second version to compare</param>
+        /// <returns>-1 if versionA is less than versionB, 0 if they're equal, 1 if versionA is greater than versionB</returns>
+        public static int Compare(string versionA, string versionB)
+        {
+            if (versionA == null) return -1;
+            if (versionB == null) return 1;
+
+            // convert version to lower, and replace all instances of "release candidate" with "rc"
+            versionA = Regex.Replace(versionA.ToLower(), @"release[\s]+candidate", "rc");
+            versionB = Regex.Replace(versionB.ToLower(), @"release[\s]+candidate", "rc");
+
+            //compare indices
+            int iVerA = 0, iVerB = 0;
+
+            int strComp;
+
+            bool lastAWasLetter = true, lastBWasLetter = true;
+
+            do
+            {
+                //store index before GetNextObject just in case we need to rollback
+                int greekIndA = iVerA;
+                int greekIndB = iVerB;
+
+                string objA = GetNextObject(versionA, ref iVerA, ref lastAWasLetter);
+                string objB = GetNextObject(versionB, ref iVerB, ref lastBWasLetter);
+
+
+                //normalize versions so comparing integer against integer, 
+                //(i.e. "1 a" is expanded to "1.0.0 a" when compared with "1.0.0 XXX")
+                //also, rollback the index on the version modified
+                if ((!lastBWasLetter && objB != null) && (objA == null || lastAWasLetter))
+                {
+                    objA = "0";
+                    iVerA = greekIndA;
+                }
+                else if ((!lastAWasLetter && objA != null) && (objB == null || lastBWasLetter))
+                {
+                    objB = "0";
+                    iVerB = greekIndB;
+                }
+
+
+                // find greek index for A and B
+                greekIndA = lastAWasLetter ? GetGreekIndex(objA) : -1;
+                greekIndB = lastBWasLetter ? GetGreekIndex(objB) : -1;
+
+
+                if (objA == null && objB == null)
+                    return 0; //versions are equal
+
+                if (objA == null) // objB != null
+                {
+                    //if versionB has a greek word, then A is greater
+                    if (greekIndB != -1)
+                        return 1;
+
+                    return -1;
+                }
+
+                if (objB == null) // objA != null
+                {
+                    //if versionA has a greek word, then B is greater
+                    if (greekIndA != -1)
+                        return -1;
+
+                    return 1;
+                }
+
+                if (char.IsDigit(objA[0]) == char.IsDigit(objB[0]))
+                {
+                    if (char.IsDigit(objA[0]))
+                    {
+                        //compare integers
+                        strComp = IntCompare(objA, objB);
+
+                        if (strComp != 0)
+                            return strComp;
+                    }
+                    else
+                    {
+                        if (greekIndA == -1 && greekIndB == -1)
+                        {
+                            //compare non-greek strings
+                            strComp = string.Compare(objA, objB);
+
+                            if (strComp != 0)
+                                return strComp;
+                        }
+                        else if (greekIndA == -1)
+                            return 1; //versionB has a greek word, thus A is newer
+                        else if (greekIndB == -1)
+                            return -1; //versionA has a greek word, thus B is newer
+                        else
+                        {
+                            //compare greek words
+                            if (greekIndA > greekIndB)
+                                return 1;
+
+                            if (greekIndB > greekIndA)
+                                return -1;
+                        }
+                    }
+                }
+                else if (char.IsDigit(objA[0]))
+                    return 1; //versionA is newer than versionB
+                else
+                    return -1; //verisonB is newer than versionA
+
+            } while (true);
+        }
+
+
+        static string GetNextObject(string version, ref int index, ref bool lastWasLetter)
+        {
+            //1 == string, 2 == int, -1 == neither
+            int StringOrInt = -1;
+
+            int startIndex = index;
+
+            while (version.Length != index)
+            {
+                if (StringOrInt == -1)
+                {
+                    if (char.IsLetter(version[index]))
+                    {
+                        startIndex = index;
+                        StringOrInt = 1;
+                    }
+                    else if (char.IsDigit(version[index]))
+                    {
+                        startIndex = index;
+                        StringOrInt = 2;
+                    }
+                    else if (lastWasLetter && !char.IsWhiteSpace(version[index]))
+                    {
+                        index++;
+                        lastWasLetter = false;
+                        return "0";
+                    }
+                }
+                else if (StringOrInt == 1 && !char.IsLetter(version[index]))
+                    break;
+                else if (StringOrInt == 2 && !char.IsDigit(version[index]))
+                    break;
+
+                index++;
+            }
+
+            //set the last "type" retrieved
+            lastWasLetter = StringOrInt == 1;
+
+            //return the retitrved sub-string
+            if (StringOrInt == 1 || StringOrInt == 2)
+                return version.Substring(startIndex, index - startIndex);
+
+            // was neither a string nor and int
+            return null;
+        }
+
+        static int GetGreekIndex(object version)
+        {
+            object val = greek_ltrs[version];
+
+            if (val == null)
+                return -1;
+
+            return (int)val;
+        }
+
+
+        static int IntCompare(string a, string b)
+        {
+            int lastZero = -1;
+
+            //Clear any preceding zeros
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] != '0')
+                    break;
+
+                lastZero = i;
+            }
+
+            if (lastZero != -1)
+                a = a.Substring(lastZero + 1, a.Length - (lastZero + 1));
+
+            lastZero = -1;
+
+            for (int i = 0; i < b.Length; i++)
+            {
+                if (b[i] != '0')
+                    break;
+
+                lastZero = i;
+            }
+
+            if (lastZero != -1)
+                b = b.Substring(lastZero + 1, b.Length - (lastZero + 1));
+
+
+            if (a.Length > b.Length)
+                return 1;
+
+            if (a.Length < b.Length)
+                return -1;
+
+            return string.Compare(a, b);
+        }
+
+        /// <summary>
+        /// Increments the version number.
+        /// </summary>
+        /// <param name="version">The version to increment.</param>
+        /// <returns>Returns the next logical verison (e.g. 1.0.2 -> 1.0.3, or 1.1 beta -> 1.1 beta 2)</returns>
+        public static string VerisonPlusPlus(string version)
+        {
+            int previ, i = 0;
+            object prevObj, obj = null;
+
+            bool junkBool = false;
+
+            do
+            {
+                previ = i;
+                prevObj = obj;
+                obj = GetNextObject(version, ref i, ref junkBool);
+
+            } while (obj != null);
+
+            if (prevObj != null)
+            {
+                // try to increment the final digit (e.g. 1.0.2 -> 1.0.3)
+                if (char.IsDigit(((string)prevObj)[0]))
+                    return version.Substring(0, previ - ((string)prevObj).Length) + NumberPlusOne((string)prevObj);
+
+                // otherwise just tack on a 2 (e.g. 1.0 beta -> 1.0 beta 2)
+                return version + " 2";
+            }
+
+            return version;
+        }
+
+        static string NumberPlusOne(string number)
+        {
+            StringBuilder sb = new StringBuilder(number.Length + 2);
+
+            int i = number.Length - 1;
+            int tempInt = 1;
+
+
+            //process the number
+            for (; i >= 0; i--)
+            {
+                tempInt += number[i] - '0';
+
+                if (tempInt == 10)
+                {
+                    sb.Insert(0, '0');
+                    tempInt = 1;
+                }
+                else
+                {
+                    sb.Insert(0, (char)(tempInt + '0'));
+                    tempInt = 0;
+                    break;
+                }
+            }
+
+            if (tempInt != 0)
+                //e.g. 99 + 1
+                sb.Insert(0, '1');
+            else if (i > 0)
+                //insert the higher digits that didn't need process
+                //e.g. 573 + 1 = 574, the leading '57' is copied over
+                sb.Insert(0, number.Substring(0, i));
+
+            return sb.ToString();
+        }
+
+
+        private static string thisVersion;
+        /// <summary>
+        /// Gets the file version of the currently executing assembly.
+        /// </summary>
+        /// <returns>The version of the currently executing assembly.</returns>
+        public static string FromExecutingAssembly()
+        {
+            if (thisVersion == null)
+                thisVersion =
+                    FileVersionInfo.GetVersionInfo(System.Windows.Forms.Application.ExecutablePath).FileVersion;
+
+            return thisVersion;
+        }
+    }
+}
---
+++ tags/release-2.5.15/frmMain/frmMain.UserElevation.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using wyUpdate.Common;
+
+namespace wyUpdate
+{
+    public partial class frmMain
+    {
+        void StartSelfElevated()
+        {
+            ProcessStartInfo psi = new ProcessStartInfo
+                                       {
+                                           ErrorDialog = true,
+                                           ErrorDialogParentHandle = Handle
+                                       };
+
+            if (SelfUpdateState == SelfUpdateState.WillUpdate)
+            {
+                //create the filename for the newly copied client
+                psi.FileName = Path.Combine(tempDirectory, Path.GetFileName(Application.ExecutablePath));
+
+                //copy self to the temp folder
+                File.Copy(Application.ExecutablePath, psi.FileName, true);
+            }
+            else if (SelfUpdateState == SelfUpdateState.FullUpdate)
+            {
+                //launch the newly updated self
+                psi.FileName = oldSelfLocation;
+            }
+            else if (isAutoUpdateMode)
+            {
+                psi.FileName = IsNewSelf ? newSelfLocation : oldSelfLocation;
+
+                // oldSelfLocation is null when elevation is needed, but no self update is taking place
+                if (string.IsNullOrEmpty(psi.FileName))
+                    psi.FileName = Application.ExecutablePath;
+            }
+            else
+                psi.FileName = Application.ExecutablePath;
+
+            if (needElevation)
+                psi.Verb = "runas"; //elevate to administrator
+
+            try
+            {
+                // write necessary info (base/temp dirs, new client files, etc.) to a file
+                SaveSelfUpdateData(Path.Combine(tempDirectory, "selfUpdate.sup"));
+
+                psi.Arguments = "-supdf:\"" + Path.Combine(tempDirectory, "selfUpdate.sup") + "\"";
+
+                Process.Start(psi);
+                Close();
+            }
+            catch (Exception ex)
+            {
+                // the process couldn't be started. This happens for 1 of 3 reasons:
+
+                // 1. The user cancelled the UAC box
+                // 2. The limited user tried to elevate to an Admin that has a blank password
+                // 3. The limited user tries to elevate as a Guest account
+                error = clientLang.AdminError;
+                errorDetails = ex.Message;
+
+                ShowFrame(Frame.Error);
+            }
+        }
+
+        bool NeedElevationToUpdate()
+        {
+            bool willSelfUpdate = (SelfUpdateState == SelfUpdateState.WillUpdate ||
+                                   SelfUpdateState == SelfUpdateState.FullUpdate ||
+                                   SelfUpdateState == SelfUpdateState.Extracted);
+
+            // no elevation necessary if it's not overwriting important files
+            if (IsAdmin || (updateFrom.InstallingTo == 0 && updateFrom.RegChanges.Count == 0 && !willSelfUpdate))
+                return false;
+
+            try
+            {
+                // if only updating local user files, no elevation is needed
+                if (OnlyUpdatingLocalUser())
+                    return false;
+
+                // UAC Shield on next button for Windows Vista+
+                if (VistaTools.AtLeastVista())
+                    VistaTools.SetButtonShield(btnNext, true);
+            }
+            catch { }
+
+            return true;
+        }
+
+        bool OnlyUpdatingLocalUser()
+        {
+            //Vista only check when the client isn't already 
+            // running with Admin (and elevated) priviledges
+
+            //Elevation is needed...
+
+            //if updating any registry other than HKEY_CURRENT_USER
+            foreach (RegChange reg in updateFrom.RegChanges)
+                if (reg.RegBasekey != RegBasekeys.HKEY_CURRENT_USER) return false;
+
+            //if installing to the system folder or one of the common folders
+            if (updateFrom.InstallingTo != 0 && (updateFrom.InstallingTo & InstallingTo.BaseDir) == 0)
+                return false;
+
+            string userProfileFolder = Environment.GetEnvironmentVariable("userprofile");
+
+            //if the basedirectory isn't in the userprofile folder (C:\Users\UserName)
+            if ((updateFrom.InstallingTo & InstallingTo.BaseDir) != 0 && !SystemFolders.IsDirInDir(userProfileFolder, baseDirectory))
+                return false;
+
+            //if the client data file isn't in the userprofile folder
+            if (!SystemFolders.IsFileInDirectory(userProfileFolder, clientFileLoc))
+                return false;
+
+            // when self-updating, if this client isn't in the userprofile folder
+            if ((SelfUpdateState == SelfUpdateState.WillUpdate
+                || SelfUpdateState == SelfUpdateState.FullUpdate
+                || SelfUpdateState == SelfUpdateState.Extracted)
+                && !SystemFolders.IsFileInDirectory(userProfileFolder, Application.ExecutablePath))
+            {
+                return false;
+            }
+
+            //it's not changing anything outside the user profile folder
+            return true;
+        }
+    }
+}
