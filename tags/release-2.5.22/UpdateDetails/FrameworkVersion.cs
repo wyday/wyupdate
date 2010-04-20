5891ae420cd7aa9ff5bd45ed6233177536c5a1b4
wyattoday <wyattoday@5120c706-1a6c-11de-8b5c-13accb87f508>
2010-04-20 22:00:39 +0000

    tag 2.5.22

---
+++ tags/release-2.5.22/UpdateDetails/FrameworkVersion.cs
@@ -0,0 +1,17 @@
+﻿public enum FrameworkVersion
+{
+    /// <summary>
+    /// The .NET Framework version was unrecognized
+    /// </summary>
+    Unknown = 0,
+
+    /// <summary>
+    /// .NET Framework 2.0 (includes .NET 3.0 & 3.5)
+    /// </summary>
+    Net2_0 = 1,
+
+    /// <summary>
+    /// .NET Framework 4.0
+    /// </summary>
+    Net4_0 = 2
+}
