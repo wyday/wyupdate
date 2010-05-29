ad8e475d9fcb9499bf9863400a316869b4fa88d3
wyattoday <wyattoday@5120c706-1a6c-11de-8b5c-13accb87f508>
2010-05-29 10:47:20 +0000

    tag 2.5.24

---
+++ tags/release-2.5.24/frmFilesInUse.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace wyUpdate
+{
+    partial class frmFilesInUse
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.AutoSize = true;
+            this.btnCancel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnCancel.Location = new System.Drawing.Point(266, 203);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(92, 22);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel Update";
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(12, 62);
+            this.txtFile.Multiline = true;
+            this.txtFile.Name = "txtFile";
+            this.txtFile.ReadOnly = true;
+            this.txtFile.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtFile.Size = new System.Drawing.Size(346, 135);
+            this.txtFile.TabIndex = 4;
+            // 
+            // frmFilesInUse
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 237);
+            this.ControlBox = false;
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.btnCancel);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(349, 178);
+            this.Name = "frmFilesInUse";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "frmFilesInUse";
+            this.Resize += new System.EventHandler(this.frmFilesInUse_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox txtFile;
+
+    }
+}
---
+++ tags/release-2.5.24/frmMain/frmMain.Downloading.cs
@@ -0,0 +1,205 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using wyUpdate.Common;
+using wyUpdate.Downloader;
+
+namespace wyUpdate
+{
+    public partial class frmMain
+    {
+        void CheckForUpdate()
+        {
+            if (!string.IsNullOrEmpty(serverOverwrite))
+            {
+                // overrite server file
+                List<string> overwriteServer = new List<string> { serverOverwrite };
+                BeginDownload(overwriteServer, 0, null, false, false);
+            }
+            else
+            {
+                //download the server file
+                BeginDownload(update.ServerFileSites, 0, null, false, false);
+            }
+        }
+
+        void DownloadUpdate()
+        {
+            if (SelfUpdateState == SelfUpdateState.FullUpdate || isAutoUpdateMode && SelfUpdateState == SelfUpdateState.WillUpdate)
+            {
+                //download self update
+                update.CurrentlyUpdating = UpdateOn.DownloadingSelfUpdate;
+                BeginSelfUpdateDownload(updateFrom.FileSites, updateFrom.Adler32);
+            }
+            else
+            {
+                //download the update file
+                update.CurrentlyUpdating = UpdateOn.DownloadingUpdate;
+                BeginDownload(updateFrom.FileSites, updateFrom.Adler32, updateFrom.SignedSHA1Hash, true, true);
+            }
+        }
+
+        //download regular update files
+        void BeginDownload(List<string> sites, long adler32, byte[] signedSHA1Hash, bool relativeProgress, bool checkSigning)
+        {
+            if (downloader != null)
+            {
+                downloader.ProgressChanged -= ShowProgress;
+                downloader.ProgressChanged -= SelfUpdateProgress;
+            }
+
+            downloader = new FileDownloader(sites, tempDirectory)
+                             {
+                                 Adler32 = adler32,
+                                 UseRelativeProgress = relativeProgress,
+                                 SignedSHA1Hash = signedSHA1Hash,
+                                 PublicSignKey = checkSigning ? update.PublicSignKey : null
+                             };
+
+            downloader.ProgressChanged += ShowProgress;
+            downloader.Download();
+        }
+
+        //download self update files (server file or update file)
+        void BeginSelfUpdateDownload(List<string> sites, long adler32)
+        {
+            if (downloader != null)
+            {
+                downloader.ProgressChanged -= ShowProgress;
+                downloader.ProgressChanged -= SelfUpdateProgress;
+            }
+
+            downloader = new FileDownloader(sites, tempDirectory)
+                             {
+                                 Adler32 = adler32
+                             };
+
+            downloader.ProgressChanged += SelfUpdateProgress;
+            downloader.Download();
+        }
+
+        //client server file downloaded
+        void DownloadClientSFSuccess()
+        {
+            //load the client server file, and see if a new version is availiable
+            ServerFile clientSF = ServerFile.Load(clientSFLoc, null, null);
+
+            //check if the client is new enough.
+            if (VersionTools.Compare(VersionTools.FromExecutingAssembly(), clientSF.NewVersion) == -1)
+            {
+                SelfUpdateState = SelfUpdateState.WillUpdate;
+
+                // autoupdate will need this SF
+                if (isAutoUpdateMode)
+                {
+                    SelfServerFile = clientSF;
+                    LoadClientServerFile();
+                }
+            }
+
+            //Show update info page
+            ShowFrame(Frame.UpdateInfo);
+        }
+
+        void LoadClientServerFile()
+        {
+            // load the self server file if it doesn't already exist
+            if (SelfServerFile == null)
+                SelfServerFile = ServerFile.Load(clientSFLoc, null, null);
+
+            updateFrom = SelfServerFile.GetVersionChoice(VersionTools.FromExecutingAssembly());
+        }
+
+        void ServerDownloadedSuccessfully()
+        {
+            //load the server file into memory
+            LoadServerFile(true);
+
+            // if we went to the finish page, bail out
+            if (frameOn != Frame.Checking)
+                return;
+
+            if (isAutoUpdateMode)
+            {
+                // set the autoupdate filename
+                autoUpdateStateFile = Path.Combine(tempDirectory, "autoupdate");
+            }
+
+            //download the client server file and see if the client is new enough
+            BeginSelfUpdateDownload(update.ClientServerSites, 0);
+        }
+
+        //returns True if an update is necessary, otherwise false
+        void LoadServerFile(bool setChangesText)
+        {
+            //load the server file
+            ServerFile = ServerFile.Load(serverFileLoc, updatePathVar, customUrlArgs);
+
+            clientLang.NewVersion = ServerFile.NewVersion;
+
+            // if no update is needed...
+            if (VersionTools.Compare(update.InstalledVersion, ServerFile.NewVersion) > -1)
+            {
+                if (isAutoUpdateMode)
+                {
+                    // send reponse that there's no update available
+                    updateHelper.SendSuccess(null, null, true, null);
+
+                    // close this client
+                    isCancelled = true;
+
+                    // let wyUpdate cleanup the files
+                    isAutoUpdateMode = false;
+
+                    // let ServerDownloadedSuccessfully() exit early
+                    frameOn = Frame.AlreadyUpToDate;
+
+                    Close();
+
+                    return;
+                }
+
+                // Show "All Finished" page
+                ShowFrame(Frame.AlreadyUpToDate);
+                return;
+            }
+
+            // get the correct update file to download
+            updateFrom = ServerFile.GetVersionChoice(update.InstalledVersion);
+
+            // if the update install the x64 system32 folder on an x86 machine we need to throw an error
+            if((updateFrom.InstallingTo & InstallingTo.SysDirx64) == InstallingTo.SysDirx64 && !SystemFolders.Is64Bit())
+            {
+                error = "Update available, but can't install 64-bit files on a 32-bit machine.";
+                errorDetails = "There's an update available (version " + ServerFile.NewVersion + "). However, this update will install files to the x64 (64-bit) system32 folder. And because this machine is an x86 (32-bit), there isn't an x64 system32 folder.";
+
+                ShowFrame(Frame.Error);
+                return;
+            }
+
+            // set the changes text
+            if (setChangesText || isAutoUpdateMode)
+            {
+                int i = ServerFile.VersionChoices.IndexOf(updateFrom);
+
+                //if there's a catch-all update start with one less than "update.VersionChoices.Count - 1"
+
+                //build the changes from all previous versions
+                for (int j = ServerFile.VersionChoices.Count - 1; j >= i; j--)
+                {
+                    //show the version number for previous updates we may have missed
+                    if (j != ServerFile.VersionChoices.Count - 1 && (!ServerFile.CatchAllUpdateExists || ServerFile.CatchAllUpdateExists && j != ServerFile.VersionChoices.Count - 2))
+                        panelDisplaying.AppendAndBoldText("\r\n\r\n" + ServerFile.VersionChoices[j + 1].Version + ":\r\n\r\n");
+
+                    // append the changes to the total changes list
+                    if (!ServerFile.CatchAllUpdateExists || ServerFile.CatchAllUpdateExists && j != ServerFile.VersionChoices.Count - 2)
+                    {
+                        if (ServerFile.VersionChoices[j].RTFChanges)
+                            panelDisplaying.AppendRichText(ServerFile.VersionChoices[j].Changes);
+                        else
+                            panelDisplaying.AppendText(ServerFile.VersionChoices[j].Changes);
+                    }
+                }
+            }
+        }
+    }
+}
