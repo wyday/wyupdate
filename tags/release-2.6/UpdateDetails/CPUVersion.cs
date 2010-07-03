bae09e39014b5bf04fb9cb10452002f739c7bd54
wyattoday <wyattoday@5120c706-1a6c-11de-8b5c-13accb87f508>
2010-07-03 08:52:28 +0000

    tag 2.6

---
+++ tags/release-2.6/UpdateDetails/CPUVersion.cs
@@ -0,0 +1,17 @@
+﻿public enum CPUVersion
+{
+    /// <summary>
+    /// A .NET assembly that is compiled with the AnyCPU flag
+    /// </summary>
+    AnyCPU = 0,
+
+    /// <summary>
+    /// A file that's built for x86 machines
+    /// </summary>
+    x86 = 1,
+
+    /// <summary>
+    /// A file that's built for x64 machines
+    /// </summary>
+    x64 = 2
+}
