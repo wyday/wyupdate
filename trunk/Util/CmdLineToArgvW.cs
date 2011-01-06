4ea97ce07a3a7f97ea497a6d2199861eedc44584
wyattoday@gmail.com <wyattoday@gmail.com@5120c706-1a6c-11de-8b5c-13accb87f508>
2011-01-06 01:52:17 +0000

    Start a windows service on AutoUpdate success/failure

---
+++ trunk/Util/CmdLineToArgvW.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace wyUpdate
+{
+    static class CmdLineToArgvW
+    {
+        // The previous examples on this page used incorrect
+        // pointer logic and were removed.
+
+        public static string[] SplitArgs(string unsplitArgumentLine)
+        {
+            int numberOfArgs;
+
+            IntPtr ptrToSplitArgs = CommandLineToArgvW(unsplitArgumentLine, out numberOfArgs);
+
+            // CommandLineToArgvW returns NULL upon failure.
+            if (ptrToSplitArgs == IntPtr.Zero)
+                throw new ArgumentException("Unable to split argument.", new Win32Exception());
+
+            // Make sure the memory ptrToSplitArgs to is freed, even upon failure.
+            try
+            {
+                string[] splitArgs = new string[numberOfArgs];
+
+                // ptrToSplitArgs is an array of pointers to null terminated Unicode strings.
+                // Copy each of these strings into our split argument array.
+                for (int i = 0; i < numberOfArgs; i++)
+                    splitArgs[i] = Marshal.PtrToStringUni(Marshal.ReadIntPtr(ptrToSplitArgs, i * IntPtr.Size));
+
+                return splitArgs;
+            }
+            finally
+            {
+                // Free memory obtained by CommandLineToArgW.
+                LocalFree(ptrToSplitArgs);
+            }
+        }
+
+        [DllImport("shell32.dll", SetLastError = true)]
+        static extern IntPtr CommandLineToArgvW([MarshalAs(UnmanagedType.LPWStr)] string lpCmdLine, out int pNumArgs);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr LocalFree(IntPtr hMem);
+    }
+}
