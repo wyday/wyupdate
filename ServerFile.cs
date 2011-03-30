5e5f806c5cf61fc64c91d1e83b4432b5ff4c14b6
wyattoday <wyatt@wyday.com>
2011-03-30 20:12:53 +0000

    Better reg elevation detection.

--- ServerFile.cs
+++ ServerFile.cs
@@ -15,11 +15,22 @@ namespace wyUpdate.Common
 
         //Determine if client elevation is needed (Vista & non-admin users)
         public InstallingTo InstallingTo = 0;
-        public List<RegChange> RegChanges = new List<RegChange>();
     }
 
     [Flags]
-    public enum InstallingTo { BaseDir = 1, SysDirx86 = 2, CommonDesktop = 4, CommonStartMenu = 8, CommonAppData = 16, SysDirx64 = 32, WindowsRoot = 64, CommonFilesx86 = 128, CommonFilesx64 = 256, ServiceOrCOMReg = 512 }
+    public enum InstallingTo {
+        BaseDir = 1, 
+        SysDirx86 = 2,
+        CommonDesktop = 4,
+        CommonStartMenu = 8,
+        CommonAppData = 16,
+        SysDirx64 = 32,
+        WindowsRoot = 64,
+        CommonFilesx86 = 128,
+        CommonFilesx64 = 256,
+        ServiceOrCOMReg = 512,
+        NonCurrentUserReg = 1024
+    }
 
     public class NoUpdatePathToNewestException : Exception { }
 
