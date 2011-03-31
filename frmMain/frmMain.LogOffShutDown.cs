1f4ab9dcec2506f9800f81dad763834d6203cc68
wyattoday <wyatt@wyday.com>
2011-03-31 15:59:33 +0000

    Explanatory comment for try{}catch{} block

--- frmMain/frmMain.LogOffShutDown.cs
+++ frmMain/frmMain.LogOffShutDown.cs
@@ -18,6 +18,13 @@ namespace wyUpdate
         {
             logOffBlocked = block;
 
+            // These API calls don't exist on pre-Vista Windows, but are
+            // *required* for Vista+ systems.
+            // Hence the try{}catch{} block. We could detect the version
+            // of Windows, but that can be spoofed by stupid users.
+
+            // Thus it's better to just try to blindly call the function
+            // and let it silently fail if it doesn't exist.
             try
             {
                 if (block)
