be2fb7f9f009540318cf5544e51209acfd7555d8
wyattoday <wyatt@wyday.com>
2012-02-09 01:26:14 +0000

    Don't crash if the close button can't be disabled/enabled.

--- SystemMenu.cs
+++ SystemMenu.cs
@@ -4,13 +4,8 @@ using System.Windows.Forms;
 
 public static class SystemMenu
 {
-    // First, we need the GetSystemMenu() function.
-    // This function does not have an Unicode counterpart
-    [DllImport("USER32", EntryPoint = "GetSystemMenu", SetLastError = true,
-               CharSet = CharSet.Unicode, ExactSpelling = true,
-               CallingConvention = CallingConvention.Winapi)]
-    static extern IntPtr GetSystemMenu(IntPtr WindowHandle,
-                                                  int bReset);
+    [DllImport("user32.dll", EntryPoint = "GetSystemMenu", SetLastError = true, CharSet = CharSet.Unicode, ExactSpelling = true, CallingConvention = CallingConvention.Winapi)]
+    static extern IntPtr GetSystemMenu(IntPtr WindowHandle, int bReset);
 
     [DllImport("user32.dll", SetLastError = true)]
     static extern int EnableMenuItem(IntPtr menu, int ideEnableItem, int enable); 
@@ -23,9 +18,13 @@ public static class SystemMenu
         //mfGrayed = 0x00000001
         //mfByCommand = 0x00000000
         IntPtr SystemMenu = GetSystemMenu(form.Handle, 0);
+        EnableMenuItem(SystemMenu, 0xF060, 1);
+
+        //NOTE: don't make this a critical failure if it doesn't work.
+        /*
         int PreviousState = EnableMenuItem(SystemMenu, 0xF060, 1);
         if (PreviousState == -1)
-            throw new Exception("The close menu does not exist");
+            throw new Exception("The close menu does not exist");*/
     }
 
     public static void EnableCloseButton(Form form)
@@ -35,8 +34,12 @@ public static class SystemMenu
         //mfString = 0x00000000
         //mfByCommand = 0x00000000
         IntPtr SystemMenu = GetSystemMenu(form.Handle, 0);
+        EnableMenuItem(SystemMenu, 0xF060, 0);
+
+        //NOTE: don't make this a critical failure if it doesn't work.
+        /*
         int PreviousState = EnableMenuItem(SystemMenu, 0xF060, 0);
         if (PreviousState == -1)
-            throw new Exception("The close menu does not exist");
+            throw new Exception("The close menu does not exist");*/
     }
 }
